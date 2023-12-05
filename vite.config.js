da90d63b10f8bdc71032a44b264ddcabc680c7ac
alexanderstanevko <dardawill@yandex.ru>
2023-12-05 12:36:50 +0200

    Add base url

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isProduction = process.env.NODE_ENV === 'production'
+const BASE_URL = isProduction ? '/colored-items/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_URL,
   plugins: [
     vue(),
   ],
